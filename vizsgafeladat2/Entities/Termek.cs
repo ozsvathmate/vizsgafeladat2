66f3b82acbc4b90e286e1975b0505e166a8c06b7
d8bog4 <d8bog4@SZA0030609>
2023-02-03 13:34:02 +0100

    Form design nezet

---
+++ vizsgafeladat2/Entities/Termek.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vizsgafeladat2.Entities
+{
+    public class Termek
+    {
+        public Guid ID { get; set; } = Guid.NewGuid();
+        public string Termeknev { get; set; }
+        public int Ar { get; set; }
+        public int Kaphato_e;
+    }
+ }
